d2d51671e8aaa933910bd203e9e0b7d1105b22a0
maheshj07 <jabademahesh2006@gmail.com>
2026-06-09 11:34:06 +0530

    Day 1 first program

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+products = []
+
 @app.route('/')
 def home():
-    return '<h1>welcome to my project</h1>'
+    return render_template('index.html')
 
-if __name__=='__main__':
+@app.route('/add', methods=['POST'])
+def add_product():
+
+    product = {
+        "name": request.form['name'],
+        "price": request.form['price'],
+        "quantity": request.form['quantity'],
+        "expiry": request.form['expiry']
+    }
+
+    products.append(product)
+
+    return redirect('/products')
+
+@app.route('/products')
+def products_page():
+    return render_template('products.html', products=products)
+
+if __name__ == '__main__':
     app.run(debug=True)
